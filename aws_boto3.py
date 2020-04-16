bc162ba7a358f6118f62faf03e2fb7fc6ba1dee2
Jeffer Zhang <zhangjeffer@gmail.com>
2020-04-15 22:01:07 -0400

    dynamodb query/add

---
+++ aws_boto3.py
@@ -0,0 +1,25 @@
+import boto3
+import json 
+
+db = boto3.resource('dynamodb')
+table = db.Table('rh_offline_users')
+
+def update_add(phone_num, ticker):
+   response = table.update_item(
+      Key={
+         'user_id': phone_num
+      },
+      UpdateExpression="ADD watchlist :c",
+      ExpressionAttributeValues={
+         ":c": set({ticker})
+      },
+      ReturnValues="UPDATED_NEW"
+   )
+
+def retrieve(phone_num):
+   response = table.get_item(
+      Key={
+         'user_id': phone_num
+      }
+   )
+   return response["Item"].get("watchlist")
